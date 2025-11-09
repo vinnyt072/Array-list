a4373090f2b40d5fb8c18447239bd95b95f731f5
vinnyt072 <vinnyt072@gmail.com>
2025-11-08 23:00:13 -0500

    Create Task class with description and completion status
    this is a class where we initialize all the primitive data types and constructors and methods

---
+++ ArrayList pojo task.java
@@ -0,0 +1,25 @@
+public class Task {
+    private String description;
+    private boolean completed;
+
+    public Task(String description) {
+        this.description = description;
+        this.completed = false;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
